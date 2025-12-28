9ac16f51593c6bc2259415662ba42c20e567f171
Dmitrii Kriskovets <156188379+dimakomplekt@users.noreply.github.com>
2025-12-29 00:45:23 +0300

    Update async_await.c

--- ESP32/async_await.c
+++ ESP32/async_await.c
@@ -10,7 +10,7 @@
 // Users must validate time values and context structures before calling the functions,
 // using appropriate checks in their own code to prevent undesired behavior or program termination.
 //
-// With this library you can't use standart delays! To pause execution, use library await command.
+// With this library you can't use standart delays! To pause execution, use library's await command.
 // 
 // !!! NOTE / IMPORTANT !!! NOTE / IMPORTANT !!! NOTE / IMPORTANT !!! NOTE / IMPORTANT !!!
 //
